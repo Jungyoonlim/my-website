97482e8132e27edf2de4ac5b19cf13153394684d
Jungyoon Lim <jungyoonlim@users.noreply.github.com>
2025-01-23 14:09:23 +0900

    cleanup

--- art.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.art-image');
-
-    images.forEach(image => {
-        console.log(image.alt); 
-    }); 
-}); 
